8fcde3b55bd9e2cf31b3ce5a96ef5f6e245642d0
santhiyagovindarajan <santhiya.govindarajan@gmail.com>
2022-09-05 18:56:53 +0530

    Third Commit

---
+++ India.java
@@ -0,0 +1,13 @@
+class India
+{
+public static void main(String[] args)
+{
+ India sandhiya = new India();
+ India krishna = new India();
+sandhiya.go();
+}
+void go()
+{
+System.out.println("Going to relative's home");
+}
+}
